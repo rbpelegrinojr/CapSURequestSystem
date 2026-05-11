0706ab37db69cb2cf596555791919329553c0922
Roden B. Pelegrino Jr. <rbpelegrinojr@capsu.edu.ph>
2026-05-11 15:11:59 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'capsu_requests');
+define('DB_USER', 'capsupon_request');
+define('DB_PASS', 'Sw0rdf1sh@214');
+define('DB_NAME', 'capsupon_request');
 define('APP_NAME', 'CapSU Request System');
-define('APP_URL', ''); // Set to your domain, e.g. https://your-domain.com
+define('APP_URL', 'https://request.capsupont.com'); // Set to your domain, e.g. https://your-domain.com
 define('ADMIN_PATH', '/admin');
-define('DEFAULT_ADMIN_EMAIL', 'admin@capsu.edu.ph');
+define('DEFAULT_ADMIN_EMAIL', 'rbpelegrinojr@capsu.edu.ph');
